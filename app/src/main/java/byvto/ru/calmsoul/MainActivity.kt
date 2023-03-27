69222bd57d231b7f304ee3562353fe36cc0c5dbd
Sergey Razrabov <vto1111@gmail.com>
2023-03-27 07:51:50 +0300

    рабочий вариант без текста

--- app/src/main/java/byvto/ru/calmsoul/MainActivity.kt
+++ app/src/main/java/byvto/ru/calmsoul/MainActivity.kt
@@ -2,7 +2,6 @@ package byvto.ru.calmsoul
 
 import android.graphics.Color
 import android.media.MediaPlayer
-import android.media.MediaPlayer.OnCompletionListener
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
@@ -12,7 +11,7 @@ import androidx.core.content.ContextCompat
 import java.io.IOException
 import kotlin.random.Random
 
-class MainActivity(var fileOggName: String = "ogg/0.ogg", var list: Array<out String>? = emptyArray(), var status: String = "start") : AppCompatActivity() {
+class MainActivity(var fileOggName: String = "0", var list: Array<out String>? = emptyArray(), var randomNumbers: List<out Int>? = emptyList()) : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         try {
@@ -22,60 +21,58 @@ class MainActivity(var fileOggName: String = "ogg/0.ogg", var list: Array<out St
             imageLogotype.setImageDrawable(
                 ContextCompat.getDrawable(this, R.drawable.tollev_white)
             )
-
-            list = assets.list("ogg")
-            setNameOggFile()
-            val afd = assets.openFd(fileOggName)
-            println(fileOggName)
-            val player = MediaPlayer()
-            player.setDataSource(afd.fileDescriptor,afd.startOffset,afd.length);
-            player.prepare()
-            clickLogo(imageLogotype, player)
+            getRadom()
+            clickLogo(imageLogotype, initMPlayer())
 
         } catch (e: IOException) {
             Log.v("List error:", "can't list")
         }
 
     }
+    fun getRadom() {
+        val countOgg = list?.count()?.toString()
+        countOgg?.let {
+            randomNumbers = List(2) { Random.nextInt(0, 3) }
+            println(it.toInt())
 
-    fun initMPlayer(): MediaPlayer {
-        println(fileOggName)
-        val afd = assets.openFd(fileOggName)
+        }
+
+    }
+    fun setFileOgg(){
+        if("ogg/" + randomNumbers!![0] + ".ogg" != fileOggName){
+            fileOggName = "ogg/" + randomNumbers!![0] + ".ogg"
+        } else {
+            fileOggName = "ogg/" + randomNumbers!![1] + ".ogg"
+        }
+    }
+    private fun initMPlayer(): MediaPlayer {
+        setFileOgg()
         val player = MediaPlayer()
-        player.reset()
-        player.setDataSource(afd.fileDescriptor,afd.startOffset,afd.length);
-        player.prepare()
+        setFileName(player)
         return player
     }
 
-    fun setNameOggFile(){
-        val countOgg = list?.count().toString()
-        val fileNumberRandom = List(2) { Random.nextInt(0, countOgg.toInt()) }
-        if(fileNumberRandom[0].toString() !== fileOggName || status == "start"){
-            fileOggName  = "ogg/" + fileNumberRandom[0] + ".ogg"
-        }
-        else{
-            fileOggName  = "ogg/" + fileNumberRandom[1] + ".ogg"
-        }
-
+    fun setFileName(player: MediaPlayer) {
+        val afd = assets.openFd(fileOggName)
+        player.reset()
+        player.setDataSource(afd.fileDescriptor,afd.startOffset,afd.length)
+        player.prepare()
     }
-    fun clickLogo(imageLogotype: ImageView, player: MediaPlayer){
-        imageLogotype.setOnClickListener {
-            if(status != "start"){
-                setNameOggFile()
-                val player = initMPlayer()
-                player.start()
-                player.setOnCompletionListener(OnCompletionListener {
-                    setBackGround()
-                })
-            } else {
-                player.start()
-                player.setOnCompletionListener(OnCompletionListener {
-                    status = ""
-                    setBackGround()
-                })
-            }
 
+    private fun clickLogo(imageLogotype: ImageView, player: MediaPlayer){
+        imageLogotype.setOnClickListener {
+
+            if (!player.isPlaying){
+                setBackGround()
+                player.start()
+            } else {
+                player.stop()
+                getRadom()
+                setFileOgg()
+                setFileName(player)
+                println(fileOggName)
+
+            }
         }
     }
 
@@ -83,9 +80,11 @@ class MainActivity(var fileOggName: String = "ogg/0.ogg", var list: Array<out St
         val contentScreen: ImageView = findViewById(R.id.contentScreen)
         val random = List(4) { Random.nextInt(0, 255) }
         val randomText: TextView = findViewById(R.id.randomText)
-        randomText.setText(":selectSound" + random[0].toString() + random[1].toString() + random[2].toString() + random[3].toString())
+        randomText.setText(random[0].toString() + random[1].toString() + random[2].toString() + random[3].toString())
         val colorScreen = Color.argb(random[0], random[1], random[2], random[3])
+//        contentScreen.setBackgroundColor(colorScreen)
         contentScreen.setBackgroundColor(colorScreen)
+
     }
 
 }
